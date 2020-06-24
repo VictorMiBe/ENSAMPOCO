15ab880749d28a36cc9751f33c0c998e0743d43b
Victor Bernabe <victoor485@gmail.com>
2020-06-24 10:52:11 -0500

    Cambie el .split por .match

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let cont = document.getElementById("contenedor")
 
 const copiardatos = () =>{
     let codigo = document.getElementById("codigo").value;
-    let pila = codigo.split(" ");
+    let pila = codigo.match(/\S+/g);
     console.log(pila);
 
     if(pila[0]==".CODE" && pila[pila.length-1]=="END"){
