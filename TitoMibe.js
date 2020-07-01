47ed5bfda30c4802fff607831492f46e8efaafbc
Victor Bernabe <victoor485@gmail.com>
2020-07-01 08:43:42 -0500

    Commit antes del merge

--- TitoMibe.js
+++ TitoMibe.js
@@ -1,7 +1,20 @@
 #Comentario desde la cuenta de TitoMibe
 #Comentario fix
+<<<<<<< HEAD
 Casdasd
 asd
 as
 da
 sd
+=======
+asdas
+da
+sd
+as
+da
+SVGFEDropShadowElementasd
+a
+sd
+asdas
+das
+>>>>>>> origin/master
